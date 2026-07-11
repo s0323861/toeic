df6d2926af6c9c706e564ee5eb1ba9f1f2b1bbf7
Akira Mukai <s0323861@gmail.com>
2026-07-11 14:13:09 +0900

    generate.phpとindex.phpの修正: TOEIC試験データの構造を更新し、受験日程の表示を改善

--- generate.php
+++ generate.php
@@ -1,63 +1,126 @@
 <?php
 
 $data = [
-
-"20260712" => [
-    "exam" => ["20260712","TOEIC受験日"],
-    "start" => ["20260507","TOEIC申込開始"],
-    "end" => ["20260603","TOEIC申込締切"],
-    "score" => ["20260730","TOEICデジタル公式認定証発行"]
-],
-
-"20260823" => [
-    "exam" => ["20260823","TOEIC受験日"],
-    "start" => ["20260521","TOEIC申込開始"],
-    "end" => ["20260713","TOEIC申込締切"],
-    "score" => ["20260910","TOEICデジタル公式認定証発行"]
-],
-
-"20260905" => [
-    "exam" => ["20260905","TOEIC受験日"],
-    "start" => ["20260604","TOEIC申込開始"],
-    "end" => ["20260729","TOEIC申込締切"],
-    "score" => ["20260925","TOEICデジタル公式認定証発行"]
-],
-
-"20260927" => [
-    "exam" => ["20260927","TOEIC受験日"],
-    "start" => ["20260714","TOEIC申込開始"],
-    "end" => ["20260814","TOEIC申込締切"],
-    "score" => ["20261015","TOEICデジタル公式認定証発行"]
-],
-
-"20261025" => [
-    "exam" => ["20261025","TOEIC受験日"],
-    "start" => ["20260729","TOEIC申込開始"],
-    "end" => ["20260910","TOEIC申込締切"],
-    "score" => ["20261112","TOEICデジタル公式認定証発行"]
-],
-
-"20261115" => [
-    "exam" => ["20261115","TOEIC受験日"],
-    "start" => ["20260817","TOEIC申込開始"],
-    "end" => ["20261005","TOEIC申込締切"],
-    "score" => ["20261204","TOEICデジタル公式認定証発行"]
-],
-
-"20261206" => [
-    "exam" => ["20261206","TOEIC受験日"],
-    "start" => ["20260911","TOEIC申込開始"],
-    "end" => ["20261026","TOEIC申込締切"],
-    "score" => ["20261223","TOEICデジタル公式認定証発行"]
-],
-
-"20261219" => [
-    "exam" => ["20261219","TOEIC受験日"],
-    "start" => ["20261006","TOEIC申込開始"],
-    "end" => ["20261111","TOEIC申込締切"],
-    "score" => ["20270108","TOEICデジタル公式認定証発行"]
-]
-
+    'lr_20260712' => [
+        'exam'  => ['20260712', 'TOEIC L&R 受験日'],
+        'start' => ['20260507', 'TOEIC L&R 申込開始'],
+        'end'   => ['20260603', 'TOEIC L&R 申込締切'],
+        'score' => ['20260730', 'TOEIC L&R デジタル公式認定証発行'],
+    ],
+    'lr_20260823' => [
+        'exam'  => ['20260823', 'TOEIC L&R 受験日'],
+        'start' => ['20260521', 'TOEIC L&R 申込開始'],
+        'end'   => ['20260713', 'TOEIC L&R 申込締切'],
+        'score' => ['20260910', 'TOEIC L&R デジタル公式認定証発行'],
+    ],
+    'lr_20260905' => [
+        'exam'  => ['20260905', 'TOEIC L&R 受験日'],
+        'start' => ['20260604', 'TOEIC L&R 申込開始'],
+        'end'   => ['20260729', 'TOEIC L&R 申込締切'],
+        'score' => ['20260925', 'TOEIC L&R デジタル公式認定証発行'],
+    ],
+    'lr_20260927' => [
+        'exam'  => ['20260927', 'TOEIC L&R 受験日'],
+        'start' => ['20260714', 'TOEIC L&R 申込開始'],
+        'end'   => ['20260814', 'TOEIC L&R 申込締切'],
+        'score' => ['20261015', 'TOEIC L&R デジタル公式認定証発行'],
+    ],
+    'lr_20261025' => [
+        'exam'  => ['20261025', 'TOEIC L&R 受験日'],
+        'start' => ['20260729', 'TOEIC L&R 申込開始'],
+        'end'   => ['20260910', 'TOEIC L&R 申込締切'],
+        'score' => ['20261112', 'TOEIC L&R デジタル公式認定証発行'],
+    ],
+    'lr_20261115' => [
+        'exam'  => ['20261115', 'TOEIC L&R 受験日'],
+        'start' => ['20260817', 'TOEIC L&R 申込開始'],
+        'end'   => ['20261005', 'TOEIC L&R 申込締切'],
+        'score' => ['20261204', 'TOEIC L&R デジタル公式認定証発行'],
+    ],
+    'lr_20261206' => [
+        'exam'  => ['20261206', 'TOEIC L&R 受験日'],
+        'start' => ['20260911', 'TOEIC L&R 申込開始'],
+        'end'   => ['20261026', 'TOEIC L&R 申込締切'],
+        'score' => ['20261223', 'TOEIC L&R デジタル公式認定証発行'],
+    ],
+    'lr_20261219' => [
+        'exam'  => ['20261219', 'TOEIC L&R 受験日'],
+        'start' => ['20261006', 'TOEIC L&R 申込開始'],
+        'end'   => ['20261111', 'TOEIC L&R 申込締切'],
+        'score' => ['20270108', 'TOEIC L&R デジタル公式認定証発行'],
+    ],
+    'sw_20260809' => [
+        'exam'  => ['20260809', 'TOEIC S&W 受験日'],
+        'start' => ['20260612', 'TOEIC S&W 申込開始'],
+        'end'   => ['20260724', 'TOEIC S&W 申込締切'],
+        'score' => ['20260826', 'TOEIC S&W 認定証発行'],
+    ],
+    'sw_20260913' => [
+        'exam'  => ['20260913', 'TOEIC S&W 受験日'],
+        'start' => ['20260717', 'TOEIC S&W 申込開始'],
+        'end'   => ['20260828', 'TOEIC S&W 申込締切'],
+        'score' => ['20260930', 'TOEIC S&W 認定証発行'],
+    ],
+    'sw_20261018' => [
+        'exam'  => ['20261018', 'TOEIC S&W 受験日'],
+        'start' => ['20260821', 'TOEIC S&W 申込開始'],
+        'end'   => ['20261002', 'TOEIC S&W 申込締切'],
+        'score' => ['20261105', 'TOEIC S&W 認定証発行'],
+    ],
+    'sw_20261129' => [
+        'exam'  => ['20261129', 'TOEIC S&W 受験日'],
+        'start' => ['20261002', 'TOEIC S&W 申込開始'],
+        'end'   => ['20261113', 'TOEIC S&W 申込締切'],
+        'score' => ['20261216', 'TOEIC S&W 認定証発行'],
+    ],
+    'sw_20261220' => [
+        'exam'  => ['20261220', 'TOEIC S&W 受験日'],
+        'start' => ['20261023', 'TOEIC S&W 申込開始'],
+        'end'   => ['20261204', 'TOEIC S&W 申込締切'],
+        'score' => ['20270107', 'TOEIC S&W 認定証発行'],
+    ],
+    'sw_20270131' => [
+        'exam'  => ['20270131', 'TOEIC S&W 受験日'],
+        'start' => ['20261204', 'TOEIC S&W 申込開始'],
+        'end'   => ['20270115', 'TOEIC S&W 申込締切'],
+        'score' => ['20270217', 'TOEIC S&W 認定証発行'],
+    ],
+    'sw_20270228' => [
+        'exam'  => ['20270228', 'TOEIC S&W 受験日'],
+        'start' => ['20270104', 'TOEIC S&W 申込開始'],
+        'end'   => ['20270212', 'TOEIC S&W 申込締切'],
+        'score' => ['20270317', 'TOEIC S&W 認定証発行'],
+    ],
+    'sw_20270328' => [
+        'exam'  => ['20270328', 'TOEIC S&W 受験日'],
+        'start' => ['20270129', 'TOEIC S&W 申込開始'],
+        'end'   => ['20270312', 'TOEIC S&W 申込締切'],
+        'score' => ['20270414', 'TOEIC S&W 認定証発行'],
+    ],
+    'bridge_106' => [
+        'exam'  => ['20260927', '第106回 TOEIC Bridge 受験日'],
+        'start' => ['20260615', '第106回 TOEIC Bridge 申込開始'],
+        'end'   => ['20260825', '第106回 TOEIC Bridge 申込締切'],
+        'score' => ['20261015', '第106回 TOEIC Bridge 認定証発行'],
+    ],
+    'bridge_107' => [
+        'exam'  => ['20261115', '第107回 TOEIC Bridge 受験日'],
+        'start' => ['20260826', '第107回 TOEIC Bridge 申込開始'],
+        'end'   => ['20261015', '第107回 TOEIC Bridge 申込締切'],
+        'score' => ['20261204', '第107回 TOEIC Bridge 認定証発行'],
+    ],
+    'bridge_108' => [
+        'exam'  => ['20270110', '第108回 TOEIC Bridge 受験日'],
+        'start' => ['20261016', '第108回 TOEIC Bridge 申込開始'],
+        'end'   => ['20261207', '第108回 TOEIC Bridge 申込締切'],
+        'score' => ['20270129', '第108回 TOEIC Bridge 認定証発行'],
+    ],
+    'bridge_109' => [
+        'exam'  => ['20270314', '第109回 TOEIC Bridge 受験日'],
+        'start' => ['20261208', '第109回 TOEIC Bridge 申込開始'],
+        'end'   => ['20270210', '第109回 TOEIC Bridge 申込締切'],
+        'score' => ['20270401', '第109回 TOEIC Bridge 認定証発行'],
+    ],
 ];
 
 header('Content-Type: text/calendar; charset=utf-8');
@@ -65,17 +128,33 @@ header('Content-Disposition: attachment; filename=toeic_schedule.ics');
 
 echo "BEGIN:VCALENDAR\r\n";
 echo "VERSION:2.0\r\n";
+echo "PRODID:-//Akira Mukai//TOEIC Schedule Maker//JA\r\n";
+echo "CALSCALE:GREGORIAN\r\n";
 
-foreach ($_POST['tests'] as $test) {
+$tests = $_POST['tests'] ?? [];
+$types = $_POST['types'] ?? [];
 
-    foreach ($_POST['types'] as $type) {
+foreach ($tests as $test) {
+    foreach ($types as $type) {
+
+        if (!isset($data[$test][$type])) {
+            continue;
+        }
 
         $date = $data[$test][$type][0];
         $title = $data[$test][$type][1];
 
+        $end_date = date(
+            'Ymd',
+            strtotime(substr($date,0,4).'-'.substr($date,4,2).'-'.substr($date,6,2).' +1 day')
+        );
+
+        $uid = md5($test . $type);
         echo "BEGIN:VEVENT\r\n";
+        echo "UID:$uid@tsukuba42195.sakura.ne.jp\r\n";
+        echo "DTSTAMP:" . gmdate('Ymd\THis\Z') . "\r\n";
         echo "DTSTART;VALUE=DATE:$date\r\n";
-        echo "DTEND;VALUE=DATE:$date\r\n";
+        echo "DTEND;VALUE=DATE:$end_date\r\n";
         echo "SUMMARY:$title\r\n";
         echo "END:VEVENT\r\n";
     }
--- index.php
+++ index.php
@@ -38,34 +38,40 @@
 
             <div class="card-body">
 
-                <div class="form-check">
-                    <input class="form-check-input"
-                           type="checkbox"
-                           checked
-                           disabled>
+                <div class="form-check mb-2">
+                    <input class="form-check-input exam-type"
+                        type="checkbox"
+                        id="type-lr"
+                        data-target="lr-area"
+                        checked>
 
-                    <label class="form-check-label">
+                    <label class="form-check-label" for="type-lr">
+                        <i class="fa-solid fa-headphones text-primary"></i>
                         TOEIC Listening & Reading
                     </label>
                 </div>
 
-                <div class="form-check">
-                    <input class="form-check-input"
-                           type="checkbox"
-                           disabled>
+                <div class="form-check mb-2">
+                    <input class="form-check-input exam-type"
+                        type="checkbox"
+                        id="type-sw"
+                        data-target="sw-area">
 
-                    <label class="form-check-label text-muted">
-                        TOEIC Speaking & Writing（準備中）
+                    <label class="form-check-label" for="type-sw">
+                        <i class="fa-solid fa-microphone text-success"></i>
+                        TOEIC Speaking & Writing
                     </label>
                 </div>
 
                 <div class="form-check">
-                    <input class="form-check-input"
-                           type="checkbox"
-                           disabled>
+                    <input class="form-check-input exam-type"
+                        type="checkbox"
+                        id="type-bridge"
+                        data-target="bridge-area">
 
-                    <label class="form-check-label text-muted">
-                        TOEIC Bridge（準備中）
+                    <label class="form-check-label" for="type-bridge">
+                        <i class="fa-solid fa-bridge text-warning"></i>
+                        TOEIC Bridge
                     </label>
                 </div>
 
@@ -143,48 +149,189 @@
 
             <div class="card-body">
 
-                <?php
+                <div id="lr-area">
 
-                $tests = [
-                    "20260712" => "2026年7月12日(日)",
-                    "20260823" => "2026年8月23日(日)",
-                    "20260905" => "2026年9月5日(土)",
-                    "20260927" => "2026年9月27日(日)",
-                    "20261025" => "2026年10月25日(日)",
-                    "20261115" => "2026年11月15日(日)",
-                    "20261206" => "2026年12月6日(日)",
-                    "20261219" => "2026年12月19日(土)"
-                ];
+                    <h5 class="mt-3 mb-3">
+                        <i class="fa-solid fa-headphones text-primary"></i>
+                        TOEIC Listening & Reading
+                    </h5>
 
-                foreach ($tests as $key => $value) {
-                    echo "
-                    <div class='form-check mb-2'>
-                        <input class='form-check-input'
-                               type='checkbox'
-                               name='tests[]'
-                               value='{$key}'>
+                    <?php
+                    $lr_tests = [
+                        "lr_20260712" => "2026年7月12日(日)",
+                        "lr_20260823" => "2026年8月23日(日)",
+                        "lr_20260905" => "2026年9月5日(土)",
+                        "lr_20260927" => "2026年9月27日(日)",
+                        "lr_20261025" => "2026年10月25日(日)",
+                        "lr_20261115" => "2026年11月15日(日)",
+                        "lr_20261206" => "2026年12月6日(日)",
+                        "lr_20261219" => "2026年12月19日(土)"
+                    ];
 
-                        <label class='form-check-label'>
-                            {$value} TOEIC L&R 公開テスト
-                        </label>
-                    </div>";
-                }
+                    foreach ($lr_tests as $key => $value) {
+                        echo "
+                        <div class='form-check mb-2'>
+                            <input class='form-check-input'
+                                type='checkbox'
+                                name='tests[]'
+                                value='{$key}'>
+                            <label class='form-check-label'>
+                                {$value} TOEIC L&R
+                            </label>
+                        </div>";
+                    }
+                    ?>
+                </div>
 
-                ?>
+                <div id="sw-area" style="display:none;">
+
+                    <h5 class="mt-4 mb-3">
+                        <i class="fa-solid fa-microphone text-success"></i>
+                        TOEIC Speaking & Writing
+                    </h5>
+
+                    <?php
+                    $sw_tests = [
+                        "sw_20260809" => "2026年8月9日(日)",
+                        "sw_20260913" => "2026年9月13日(日)",
+                        "sw_20261018" => "2026年10月18日(日)",
+                        "sw_20261129" => "2026年11月29日(日)",
+                        "sw_20261220" => "2026年12月20日(日)",
+                        "sw_20270131" => "2027年1月31日(日)",
+                        "sw_20270228" => "2027年2月28日(日)",
+                        "sw_20270328" => "2027年3月28日(日)"
+                    ];
+
+                    foreach ($sw_tests as $key => $value) {
+                        echo "
+                        <div class='form-check mb-2'>
+                            <input class='form-check-input'
+                                type='checkbox'
+                                name='tests[]'
+                                value='{$key}'>
+                            <label class='form-check-label'>
+                                {$value} TOEIC S&W
+                            </label>
+                        </div>";
+                    }
+                    ?>
+                </div>
+
+                <div id="bridge-area" style="display:none;">
+
+                    <h5 class="mt-4 mb-3">
+                        <i class="fa-solid fa-bridge text-warning"></i>
+                        TOEIC Bridge
+                    </h5>
+
+                    <?php
+                    $bridge_tests = [
+                        "bridge_106" => "第106回 TOEIC Bridge",
+                        "bridge_107" => "第107回 TOEIC Bridge",
+                        "bridge_108" => "第108回 TOEIC Bridge",
+                        "bridge_109" => "第109回 TOEIC Bridge"
+                    ];
+
+                    foreach ($bridge_tests as $key => $value) {
+                        echo "
+                        <div class='form-check mb-2'>
+                            <input class='form-check-input'
+                                type='checkbox'
+                                name='tests[]'
+                                value='{$key}'>
+                            <label class='form-check-label'>
+                                {$value}
+                            </label>
+                        </div>";
+                    }
+                    ?>
+                </div>
 
             </div>
         </div>
 
         <div class="text-center">
-            <button class="btn btn-primary btn-lg">
+            <button id="submitBtn"
+                class="btn btn-primary btn-lg w-100"
+                disabled>
                 <i class="fa-brands fa-google"></i>
                 Googleカレンダーへ追加
             </button>
+            <div id="message"
+                class="text-muted text-center mt-2">
+                受験回を1つ以上選択してください。
+            </div>
         </div>
 
     </form>
 
 </div>
 
+<script>
+document.querySelectorAll('.exam-type').forEach(function(checkbox){
+
+    checkbox.addEventListener('change', function(){
+
+        const target =
+            document.getElementById(
+                this.dataset.target
+            );
+
+        if(this.checked){
+            target.style.display = 'block';
+        }else{
+            target.style.display = 'none';
+
+            target.querySelectorAll(
+                'input[type="checkbox"]'
+            ).forEach(function(cb){
+                cb.checked = false;
+            });
+        }
+
+    });
+
+});
+
+function updateSubmitButton() {
+
+    const checkedTests =
+        document.querySelectorAll(
+            'input[name="tests[]"]:checked'
+        );
+
+    const submitBtn =
+        document.getElementById(
+            'submitBtn'
+        );
+
+    const message =
+        document.getElementById(
+            'message'
+        );
+
+    if (checkedTests.length > 0) {
+        submitBtn.disabled = false;
+        message.style.display = 'none';
+    } else {
+        submitBtn.disabled = true;
+        message.style.display = 'block';
+    }
+}
+
+document.querySelectorAll(
+    'input[name="tests[]"]'
+).forEach(function(cb) {
+
+    cb.addEventListener(
+        'change',
+        updateSubmitButton
+    );
+
+});
+
+updateSubmitButton();
+
+</script>
 </body>
 </html>
