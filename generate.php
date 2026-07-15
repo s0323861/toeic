e7ac13461e3173be68f7fd70fb70af7e54533ddb
Akira Mukai <s0323861@gmail.com>
2026-07-15 21:44:01 +0900

    Update TOEIC schedule data format
    Refactor data structure for TOEIC schedule events.

--- generate.php
+++ generate.php
@@ -1,127 +1,14 @@
 <?php
 
-$data = [
-    'lr_20260712' => [
-        'exam'  => ['20260712', 'TOEIC L&R 受験日'],
-        'start' => ['20260507', 'TOEIC L&R 申込開始'],
-        'end'   => ['20260603', 'TOEIC L&R 申込締切'],
-        'score' => ['20260730', 'TOEIC L&R デジタル公式認定証発行'],
-    ],
-    'lr_20260823' => [
-        'exam'  => ['20260823', 'TOEIC L&R 受験日'],
-        'start' => ['20260521', 'TOEIC L&R 申込開始'],
-        'end'   => ['20260713', 'TOEIC L&R 申込締切'],
-        'score' => ['20260910', 'TOEIC L&R デジタル公式認定証発行'],
-    ],
-    'lr_20260905' => [
-        'exam'  => ['20260905', 'TOEIC L&R 受験日'],
-        'start' => ['20260604', 'TOEIC L&R 申込開始'],
-        'end'   => ['20260729', 'TOEIC L&R 申込締切'],
-        'score' => ['20260925', 'TOEIC L&R デジタル公式認定証発行'],
-    ],
-    'lr_20260927' => [
-        'exam'  => ['20260927', 'TOEIC L&R 受験日'],
-        'start' => ['20260714', 'TOEIC L&R 申込開始'],
-        'end'   => ['20260814', 'TOEIC L&R 申込締切'],
-        'score' => ['20261015', 'TOEIC L&R デジタル公式認定証発行'],
-    ],
-    'lr_20261025' => [
-        'exam'  => ['20261025', 'TOEIC L&R 受験日'],
-        'start' => ['20260729', 'TOEIC L&R 申込開始'],
-        'end'   => ['20260910', 'TOEIC L&R 申込締切'],
-        'score' => ['20261112', 'TOEIC L&R デジタル公式認定証発行'],
-    ],
-    'lr_20261115' => [
-        'exam'  => ['20261115', 'TOEIC L&R 受験日'],
-        'start' => ['20260817', 'TOEIC L&R 申込開始'],
-        'end'   => ['20261005', 'TOEIC L&R 申込締切'],
-        'score' => ['20261204', 'TOEIC L&R デジタル公式認定証発行'],
-    ],
-    'lr_20261206' => [
-        'exam'  => ['20261206', 'TOEIC L&R 受験日'],
-        'start' => ['20260911', 'TOEIC L&R 申込開始'],
-        'end'   => ['20261026', 'TOEIC L&R 申込締切'],
-        'score' => ['20261223', 'TOEIC L&R デジタル公式認定証発行'],
-    ],
-    'lr_20261219' => [
-        'exam'  => ['20261219', 'TOEIC L&R 受験日'],
-        'start' => ['20261006', 'TOEIC L&R 申込開始'],
-        'end'   => ['20261111', 'TOEIC L&R 申込締切'],
-        'score' => ['20270108', 'TOEIC L&R デジタル公式認定証発行'],
-    ],
-    'sw_20260809' => [
-        'exam'  => ['20260809', 'TOEIC S&W 受験日'],
-        'start' => ['20260612', 'TOEIC S&W 申込開始'],
-        'end'   => ['20260724', 'TOEIC S&W 申込締切'],
-        'score' => ['20260826', 'TOEIC S&W 認定証発行'],
-    ],
-    'sw_20260913' => [
-        'exam'  => ['20260913', 'TOEIC S&W 受験日'],
-        'start' => ['20260717', 'TOEIC S&W 申込開始'],
-        'end'   => ['20260828', 'TOEIC S&W 申込締切'],
-        'score' => ['20260930', 'TOEIC S&W 認定証発行'],
-    ],
-    'sw_20261018' => [
-        'exam'  => ['20261018', 'TOEIC S&W 受験日'],
-        'start' => ['20260821', 'TOEIC S&W 申込開始'],
-        'end'   => ['20261002', 'TOEIC S&W 申込締切'],
-        'score' => ['20261105', 'TOEIC S&W 認定証発行'],
-    ],
-    'sw_20261129' => [
-        'exam'  => ['20261129', 'TOEIC S&W 受験日'],
-        'start' => ['20261002', 'TOEIC S&W 申込開始'],
-        'end'   => ['20261113', 'TOEIC S&W 申込締切'],
-        'score' => ['20261216', 'TOEIC S&W 認定証発行'],
-    ],
-    'sw_20261220' => [
-        'exam'  => ['20261220', 'TOEIC S&W 受験日'],
-        'start' => ['20261023', 'TOEIC S&W 申込開始'],
-        'end'   => ['20261204', 'TOEIC S&W 申込締切'],
-        'score' => ['20270107', 'TOEIC S&W 認定証発行'],
-    ],
-    'sw_20270131' => [
-        'exam'  => ['20270131', 'TOEIC S&W 受験日'],
-        'start' => ['20261204', 'TOEIC S&W 申込開始'],
-        'end'   => ['20270115', 'TOEIC S&W 申込締切'],
-        'score' => ['20270217', 'TOEIC S&W 認定証発行'],
-    ],
-    'sw_20270228' => [
-        'exam'  => ['20270228', 'TOEIC S&W 受験日'],
-        'start' => ['20270104', 'TOEIC S&W 申込開始'],
-        'end'   => ['20270212', 'TOEIC S&W 申込締切'],
-        'score' => ['20270317', 'TOEIC S&W 認定証発行'],
-    ],
-    'sw_20270328' => [
-        'exam'  => ['20270328', 'TOEIC S&W 受験日'],
-        'start' => ['20270129', 'TOEIC S&W 申込開始'],
-        'end'   => ['20270312', 'TOEIC S&W 申込締切'],
-        'score' => ['20270414', 'TOEIC S&W 認定証発行'],
-    ],
-    'bridge_106' => [
-        'exam'  => ['20260927', '第106回 TOEIC Bridge 受験日'],
-        'start' => ['20260615', '第106回 TOEIC Bridge 申込開始'],
-        'end'   => ['20260825', '第106回 TOEIC Bridge 申込締切'],
-        'score' => ['20261015', '第106回 TOEIC Bridge 認定証発行'],
-    ],
-    'bridge_107' => [
-        'exam'  => ['20261115', '第107回 TOEIC Bridge 受験日'],
-        'start' => ['20260826', '第107回 TOEIC Bridge 申込開始'],
-        'end'   => ['20261015', '第107回 TOEIC Bridge 申込締切'],
-        'score' => ['20261204', '第107回 TOEIC Bridge 認定証発行'],
-    ],
-    'bridge_108' => [
-        'exam'  => ['20270110', '第108回 TOEIC Bridge 受験日'],
-        'start' => ['20261016', '第108回 TOEIC Bridge 申込開始'],
-        'end'   => ['20261207', '第108回 TOEIC Bridge 申込締切'],
-        'score' => ['20270129', '第108回 TOEIC Bridge 認定証発行'],
-    ],
-    'bridge_109' => [
-        'exam'  => ['20270314', '第109回 TOEIC Bridge 受験日'],
-        'start' => ['20261208', '第109回 TOEIC Bridge 申込開始'],
-        'end'   => ['20270210', '第109回 TOEIC Bridge 申込締切'],
-        'score' => ['20270401', '第109回 TOEIC Bridge 認定証発行'],
-    ],
-];
+$schedules = require 'schedule_data.php';
+
+$data = [];
+
+foreach ($schedules as $group) {
+    foreach ($group['events'] as $key => $event) {
+        $data[$key] = $event;
+    }
+}
 
 header('Content-Type: text/calendar; charset=utf-8');
 header('Content-Disposition: attachment; filename=toeic_schedule.ics');
